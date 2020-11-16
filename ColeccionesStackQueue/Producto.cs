b501673479eb29915f88c2f143129a9c19af9d25
JAVIER <javi_hen@hotmail.com>
2020-11-16 12:14:33 -0400

    Agregar archivos de proyecto.

---
+++ ColeccionesStackQueue/Producto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColeccionesStackQueue
+{
+    public class Producto
+    {
+        public String Descripcion;
+
+        public Producto()
+        {
+            this.Descripcion = "Esta vacio";
+        }
+
+        public Producto(String d)
+        {
+            this.Descripcion = d;
+        }
+
+
+        public String GetDescripcion()
+        {
+            return this.Descripcion;
+        }
+        public void SetDescripcion(String Dato)
+        {
+            this.Descripcion = Dato;
+        }
+    }
+}
---
+++ ColeccionesStackQueue/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColeccionesStackQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Producto objP1 = new Producto("Pantallas");
+            Producto objP2 = new Producto("Mouse");
+            Producto objP3 = new Producto();
+            // --------- COLECCION STACK ------------ ultimo en entrar, primero en salir
+            Stack<Producto> aProducto = new Stack<Producto>();
+
+            aProducto.Push(objP1);
+            aProducto.Push(objP2);
+            aProducto.Push(objP3);
+
+            foreach (Producto P in aProducto)
+            {
+                Console.WriteLine(P.Descripcion);
+            }
+
+            Console.WriteLine(aProducto.Peek().Descripcion);
+
+            aProducto.Pop();
+
+            foreach (Producto P in aProducto)
+            {
+                Console.WriteLine(P.Descripcion);
+            }
+            //--------- CLASE COLECCION QUEUE ------------ Primero en entrar, primero en salir.
+
+            Queue<Producto> aProducto2 = new Queue<Producto>();
+
+            aProducto2.Enqueue(objP1);
+            aProducto2.Enqueue(objP2);
+            aProducto2.Enqueue(objP3);
+            
+            foreach (Producto P in aProducto2)
+            {
+                Console.WriteLine(P.Descripcion);
+            }
+
+            Console.WriteLine("--------------------------------");
+
+            Console.WriteLine(aProducto2.Peek().Descripcion);
+
+            Console.WriteLine("--------------------------------");
+
+            aProducto2.Dequeue();
+
+            foreach (Producto P in aProducto2)
+            {
+                Console.WriteLine(P.Descripcion);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
